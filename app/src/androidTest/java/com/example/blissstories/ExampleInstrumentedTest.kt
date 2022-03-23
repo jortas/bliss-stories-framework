178cacb85c22caf92feeb670f8ceca5e7343a9a2
jortas <jhortas@blissapplications.com>
2022-03-23 17:32:32 +0000

    change from moeyslider to blissStories

--- app/src/androidTest/java/com/example/blissstories/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/blissstories/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.moeyslider", appContext.packageName)
+        assertEquals("com.example.blissstories", appContext.packageName)
     }
 }
